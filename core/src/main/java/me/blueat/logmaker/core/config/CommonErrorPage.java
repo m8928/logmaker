cd705a42995982497bbcacf97ba14bf3ed7feb70
m8928 <m8928@me.com>
2023-02-13 00:09:04 +0900

    Add ui

---
+++ core/src/main/java/me/blueat/logmaker/core/config/CommonErrorPage.java
@@ -0,0 +1,16 @@
+package me.blueat.logmaker.core.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonErrorPage implements ErrorPageRegistrar {
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage e404 = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+        registry.addErrorPages(e404);
+    }
+}
